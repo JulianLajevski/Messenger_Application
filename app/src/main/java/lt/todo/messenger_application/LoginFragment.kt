035f1aef37591c8736787bcd48fe58d4d3d8073d
Julian Lajevski <julian.lajevski@gmail.com>
2021-03-28 20:59:46 +0300

    Login logic created.

--- app/src/main/java/lt/todo/messenger_application/LoginFragment.kt
+++ app/src/main/java/lt/todo/messenger_application/LoginFragment.kt
@@ -5,15 +5,59 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.fragment_login.*
 
 
 class LoginFragment : Fragment() {
 
+    private lateinit var auth: FirebaseAuth
+    private lateinit var firebaseUser: FirebaseUser
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        auth = FirebaseAuth.getInstance()
+        firebaseUser = auth.currentUser!!
+
+        initListeners()
+    }
+
+    private fun initListeners(){
+
+        registerButton.setOnClickListener {
+
+        }
+
+        loginButton.setOnClickListener {
+            val email: String = emailEditText.text.toString()
+            val password: String = passwordEditText.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password).addOnCompleteListener{ task ->
+                if(task.isSuccessful){
+                    Toast.makeText(
+                        requireContext(),
+                        "You're logged successfully!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                }else{
+                    Toast.makeText(
+                        requireContext(),
+                        task.exception!!.message.toString(),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+
     }
 }
