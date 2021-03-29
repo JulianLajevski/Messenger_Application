de567e297d4e676cd9a0f3bd3ca5134546d90633
Julian Lajevski <julian.lajevski@gmail.com>
2021-03-29 11:42:30 +0300

    Done with getUsersList method.

--- app/src/main/java/lt/todo/messenger_application/HomeFragment.kt
+++ app/src/main/java/lt/todo/messenger_application/HomeFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.fragment_home_fragment.*
 
 
@@ -24,8 +28,34 @@ class HomeFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
         userRecyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        getUsersList()
     }
 
+    private fun getUsersList(){
+        val firebase: FirebaseUser = FirebaseAuth.getInstance().currentUser!!
+        val databaseReference: DatabaseReference = FirebaseDatabase.getInstance().getReference("Users")
 
+        databaseReference.addValueEventListener(object: ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                userList.clear()
+
+                for( dataSnapShot: DataSnapshot in snapshot.children ){
+                    val user = dataSnapShot.getValue(User::class.java)
+
+                    if(!user!!.userId.equals(firebase.uid)){
+                        userList.add(user)
+                    }
+                }
+
+                val userAdapter = UserAdapter(requireContext(), userList)
+                userRecyclerView.adapter = userAdapter
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(requireContext(), error.message, Toast.LENGTH_SHORT).show()
+            }
+
+        })
+    }
 
 }
