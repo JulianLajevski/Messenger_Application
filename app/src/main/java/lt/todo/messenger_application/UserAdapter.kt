df1d14d398289383c35eea44374647db9efa5571
Julian Lajevski <julian.lajevski@gmail.com>
2021-03-29 11:15:38 +0300

    Created ViewHolder class in UserAdapter and implement necessary methods.

--- app/src/main/java/lt/todo/messenger_application/UserAdapter.kt
+++ app/src/main/java/lt/todo/messenger_application/UserAdapter.kt
@@ -1,6 +1,26 @@
 package lt.todo.messenger_application
 
 import android.content.Context
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 
-class UserAdapter(private val context: Context, private val userList: ArrayList<User>) {
+class UserAdapter(private val context: Context, private val userList: ArrayList<User>): RecyclerView.Adapter<UserAdapter.ViewHolder>() {
+
+    class ViewHolder {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+
 }
