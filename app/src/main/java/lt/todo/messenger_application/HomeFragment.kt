81694a3f4220c2cf9098b11a1aefce78db2d0dcc
Julian Lajevski <julian.lajevski@gmail.com>
2021-03-29 11:36:51 +0300

    Setup users recycler view.

--- app/src/main/java/lt/todo/messenger_application/HomeFragment.kt
+++ app/src/main/java/lt/todo/messenger_application/HomeFragment.kt
@@ -5,16 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_home_fragment.*
 
 
 class HomeFragment : Fragment() {
 
+    var userList = ArrayList<User>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        userRecyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
     }
 
 
