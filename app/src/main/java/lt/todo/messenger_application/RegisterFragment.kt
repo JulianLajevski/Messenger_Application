540caf9ad2d29f3ce36d3d5f8804f85742ae9448
Julian Lajevski <julian.lajevski@gmail.com>
2021-04-06 22:33:17 +0300

    Completed with register view model.

--- app/src/main/java/lt/todo/messenger_application/RegisterFragment.kt
+++ app/src/main/java/lt/todo/messenger_application/RegisterFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContentProviderCompat
+import androidx.lifecycle.ViewModelProvider
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
@@ -17,8 +19,13 @@ import kotlinx.android.synthetic.main.fragment_register.*
 
 class RegisterFragment : Fragment() {
 
-    private lateinit var databaseReference: DatabaseReference
-    private lateinit var auth: FirebaseAuth
+    private lateinit var registerViewModel: RegisterViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        registerViewModel = ViewModelProvider(requireActivity()).get(RegisterViewModel::class.java)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -29,60 +36,49 @@ class RegisterFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        auth = FirebaseAuth.getInstance()
+
 
         initListeners()
     }
 
     private fun initListeners(){
         signUpButton.setOnClickListener {
-            val email: String = email.text.toString()
-            val password: String = password.text.toString()
-            val confirmPassword: String = passwordConfirm.text.toString()
-            val name: String = name.text.toString()
+            val email = email.text.toString()
+            val password = password.text.toString()
+            val name = name.text.toString()
+            val confirmPassword = passwordConfirm.text.toString()
 
-            if(password == confirmPassword){
-                FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password).addOnCompleteListener(
-                    OnCompleteListener<AuthResult> { task ->
-                        if(task.isSuccessful){
-                            val firebaseUser: FirebaseUser = task.result!!.user!!
-                            Toast.makeText(
-                                requireContext(),
-                                "Registered!",
-                                Toast.LENGTH_SHORT
-                            ).show()
-
-                            val user: FirebaseUser? = auth.currentUser
-                            val userId: String = user!!.uid
-
-                            databaseReference = FirebaseDatabase.getInstance().getReference("Users").child(userId)
-
-                            val hashMap: HashMap<String, String> = HashMap()
-                            hashMap.put("userId", userId)
-                            hashMap.put("name", name)
-                            hashMap.put("email", email)
-                            hashMap.put("password", password)
-                            hashMap.put("profileImage", "")
-
-                            databaseReference.setValue(hashMap)
-
-                        }else{
-                            Toast.makeText(
-                                requireContext(),
-                                task.exception!!.message.toString(),
-                                Toast.LENGTH_SHORT
-                            ).show()
+            if(email.isNotBlank() && password.isNotBlank() && name.isNotBlank() && confirmPassword.isNotBlank()){
+                if(password == confirmPassword){
+                    registerViewModel.register(email, password, name)
+                    registerViewModel.authResult.observe(viewLifecycleOwner, { result ->
+                        when (result){
+                            true ->{
+                                Toast.makeText(
+                                        requireContext(),
+                                        "Registered!",
+                                        Toast.LENGTH_SHORT
+                                ).show()
+                            }
+                            false ->{
+                                registerViewModel.authErrorMessage.observe(viewLifecycleOwner,{ errorMsg ->
+                                    Toast.makeText(
+                                            requireContext(),
+                                            errorMsg,
+                                            Toast.LENGTH_SHORT
+                                    ).show()
+                                })
+                            }
                         }
-                    }
-                )
-            }else{
-                Toast.makeText(
-                    requireContext(),
-                    "Password not same!",
-                    Toast.LENGTH_SHORT
-                ).show()
+                    })
+                }else{
+                    Toast.makeText(
+                            requireContext(),
+                            "Password not same!",
+                            Toast.LENGTH_SHORT
+                    ).show()
+                }
             }
-
 
         }
     }
